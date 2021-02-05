245f0ceb02c25a6dc0761a950d558f61fd41ad7f
abasset <jusal@DESKTOP-QSBLA1H.localdomain>
2021-02-05 15:03:57 +0100

    Création classe tetrimino

---
+++ src/main/java/tetris/tetrimino.java
@@ -0,0 +1,12 @@
+package tetris;
+
+import java.util.ArrayList;
+public class tetrimino {
+	protected int posx ;
+	protected int posy ;
+	protected int orientation ;
+	protected int[][][] shape = new int [4][4][4] ;
+	tetrimino(){};
+			
+}
+ //{ { { {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0} },{ {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0} },{ {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0} },{ {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0} } },};
